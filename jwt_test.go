d383a808c842bc0593cf6e44497cb71401dc1d27
Ståle Dahl <stalehd@telenordigital.com>
2017-10-06 13:57:05 +0200

    Add Apache 2.0 license

--- jwt_test.go
+++ jwt_test.go
@@ -1,5 +1,20 @@
 package goconnect
 
+/*
+**   Copyright 2017 Telenor Digital AS
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**      http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+ */
 import "testing"
 
 const connectTestData = `eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI1OTg0MzU1Nzg2MTAzNDYzOTM2IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFtciI6WyJVSURfUFdEIl0sImlzcyI6Imh0dHBzOlwvXC9jb25uZWN0LnN0YWdpbmcudGVsZW5vcmRpZ2l0YWwuY29tXC9vYXV0aCIsInBob25lX251bWJlcl92ZXJpZmllZCI6ZmFsc2UsInRkX3NscyI6ZmFsc2UsImxvY2FsZSI6ImVuIiwiYWNyIjoiMiIsImF1ZCI6WyJ0ZWxlbm9yZGlnaXRhbC1sb3JhY29yZS13ZWIiXSwidGRfYXUiOiI0NzkyNDA0NjEwIiwiYXV0aF90aW1lIjoxNDg3NjA2NTQ3LCJuYW1lIjoiU3TDpWxlIEZvbyDwn5iA8J-YgfCfmIgiLCJwaG9uZV9udW1iZXIiOiI0NzkyNDA0NjEwIiwiZXhwIjoxNDg3NjEwNDQ3LCJpYXQiOjE0ODc2MDY1NDcsImVtYWlsIjoic3RhbGVoZCtmb29iYXJiYXpAY29tb3lvLmNvbSJ9.boOztuLNDIdLnO_YDvxtMQM37wWPhJDqi3UF-CvUfpflFZ0flZHxAQ0lCyqecJDoHw72EM3rlXy95w4lMThDPXyFvsw_2zmSQTar9gK03W12JuLPsfEP9Oo4NHwHkYKpEr5pfoLYIbwOMJxTx-j9VQ_9jmdbdbezE2TdHlzwgpg`
